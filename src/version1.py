1fdc9389ee7c8e2f7b2a1724b448d667d8075b71
askarkg12 <askarkg12@gmail.com>
2018-06-27 13:25:55 +0100

    Now you can find the position

--- src/version1.py
+++ src/version1.py
@@ -9,7 +9,7 @@ def updateTh(x):
 cv2.namedWindow('Beetroot', cv2.WINDOW_NORMAL)
 cv2.createTrackbar('Sat','Beetroot',22,255,updateTh)
 cv2.createTrackbar('Gray','Beetroot',22,255,updateTh)
-cv2.createTrackbar('GoodBad','Beetroot',28,100,updateTh)
+cv2.createTrackbar('GoodBad','Beetroot',28,500,updateTh)
 cv2.namedWindow('Saturation', cv2.WINDOW_NORMAL)
 cv2.namedWindow('Threshold', cv2.WINDOW_NORMAL)
 
@@ -17,6 +17,12 @@ cap = cv2.VideoCapture(1)
 
 while(True):
     ret, img = cap.read()
+
+    img = img[:, 0:520]
+
+    height, width, channels = img.shape
+
+    print(str(width) + "x" + str(height))
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     grayTh = cv2.getTrackbarPos('Gray', 'Beetroot')
@@ -43,15 +49,22 @@ while(True):
             maxSize = cv2.contourArea(cnt)
             maxId = id
         id = id + 1
-    print(maxSize)
-    print(maxId)
+    #print(maxSize)
+    #print(maxId)
     goodBad = cv2.getTrackbarPos('GoodBad', 'Beetroot')
-    if maxSize > goodBad*1000:
+    if maxSize > goodBad*100:
         #print("This is bad")
         cv2.circle(img,(20, 20), 20, (0,0,225),-1)
     else:
-        print("This is good")
+        #print("This is good")
         cv2.circle(img,(20, 20), 20, (0,225,0),-1)
+    try:
+        M = cv2.moments(contours[maxId])
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        cv2.circle(img, (cX, cY), 7, (255, 255, 255), -1)
+    except:
+        print("Did not find anything")
     cv2.drawContours(img, contours, maxId, (0,255,0), 3)
 
     cv2.imshow("Beetroot", img)
